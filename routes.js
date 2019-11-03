c2f520aded7450e51cd35764404e74da8293d988
Jedidias Zapata <jedy_z_31@hotmail.com>
2019-11-03 15:35:26 -0500

    tienda general maquetada

---
+++ routes.js
@@ -0,0 +1,35 @@
+const assert = require('assert');
+const ObjectID = require('mongodb').ObjectID;
+
+function createRoutes(app, db) {
+
+    app.get('/', (request, response) => {
+        console.log('Alguien entró a la ruta inicial');
+        response.sendFile(__dirname + '/public/index.html');
+    });
+
+    // app.get('/tiendass', (request, response) => {
+    //     console.log('Alguien entró a la tienda');
+    //     response.render('store');
+    // });
+
+    app.get('/tienda', (request, response) => {
+        const products = db.collection('products');
+        console.log('Alguien entró a la tienda');
+
+        //buscamos todos los productos
+        products.find({})
+            //transformamos el cursor a una arreglo
+            .toArray((err, result) => {
+                //aseguramos de que no hay error
+                assert.equal(null, err);
+
+                var context = {
+                    products: result
+                };
+
+                response.render('store', context);
+            });
+    });
+}
+module.exports = createRoutes;
